9b8232a7c93992004e3dcbc5b53e872f372a7f9b
Roberto Ricci <roberto.ricci@ricci-consulting.com>
2025-07-07 16:28:08 +0200

    Fix less per wpcli

--- lib/wp-cli/rootfs/build/setup-wp-cli.sh
+++ lib/wp-cli/rootfs/build/setup-wp-cli.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+apt-get install -y --no-install-recommends less
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
